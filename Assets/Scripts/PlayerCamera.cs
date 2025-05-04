7f2eb458fc661f1dffb84a69ce55edacd43d08f5
dylanHilliard <ghilliard69420@gmail.com>
2025-05-04 18:34:13 -0500

    fixed camera

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -5,7 +5,7 @@ public class PlayerCamera : MonoBehaviour
 
     public Camera playerCamera;
     private GameObject player;
-    public float lookSpeed = 2.0f;
+    public float lookSpeed = 700.0f;
     private Vector2 rotation = Vector2.zero;
 
 
@@ -22,15 +22,17 @@ public class PlayerCamera : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        rotation.x = playerCamera.transform.localEulerAngles.y + Input.GetAxisRaw("Mouse X") * lookSpeed;
-        rotation.y = Mathf.Clamp(playerCamera.transform.localEulerAngles.z + Input.GetAxis("Mouse Y")* lookSpeed, -90f, 90f);
-
-        playerCamera.transform.localEulerAngles = new Vector3(-rotation.y, rotation.x, 0);
+        /*
+        rotation.y = playerCamera.transform.localEulerAngles.y + Input.GetAxisRaw("Mouse X") * lookSpeed *Time.deltaTime;
+        rotation.x = playerCamera.transform.localEulerAngles.x + Input.GetAxisRaw("Mouse Y")* lookSpeed *Time.deltaTime;
+          playerCamera.transform.localEulerAngles = new Vector3(-rotation.x, rotation.y, 0);
         Vector3 playerRotation = player.transform.localEulerAngles+ new Vector3(0, Input.GetAxisRaw("Mouse X") * lookSpeed, 0);
-        player.transform.localEulerAngles = new Vector3(playerRotation.x, playerRotation.y, playerRotation.z);
-
+        player.transform.localEulerAngles = new Vector3(playerRotation.x, rotation.x, playerRotation.z);
+        */
+        playerCamera.transform.Rotate(-Input.GetAxisRaw("Mouse Y") * lookSpeed * Time.deltaTime, 0f, 0f, Space.Self);
+        player.transform.Rotate(0f, Input.GetAxisRaw("Mouse X") * lookSpeed * Time.deltaTime, 0f, Space.Self);
         playerCamera.transform.position = player.transform.position;
     }
 }
